62510b5ff3329dade72bf03445d42cee3188dca7
Karenqzs <43148343+Karenqzs@users.noreply.github.com>
2018-10-03 21:33:42 -0700

    Add files via upload

---
+++ 100 times.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+	colorMode(HSB, width, height, 100);
+}
+
+function draw() {
+
+  for (var x=0; x< width; x+= 40 ){
+    for (var y=0; y< height; y+= 40) {
+	    stroke(x, y, 100);
+	    point(random(x), random(y));
+}
+  }
+}
