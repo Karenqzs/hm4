1beb4b217f9eb5f43c5feb1103d7df6b341ea128
Karenqzs <43148343+Karenqzs@users.noreply.github.com>
2018-10-05 13:07:05 -0700

    Update 100 times.js

--- 100 times.js
+++ 100 times.js
@@ -8,7 +8,7 @@ function draw() {
   for (var x=0; x< width; x+= 40 ){
     for (var y=0; y< height; y+= 40) {
 	    stroke(x, y, 100);
-	    point(random(x), random(y));
+	    point(random(x,x+40), random(y,y+40));
 }
   }
 }
