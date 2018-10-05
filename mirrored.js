2e7938b327eaa2c364439cfbca866e0647c3eb16
Karenqzs <43148343+Karenqzs@users.noreply.github.com>
2018-10-05 13:35:57 -0700

    Update mirrored.js

--- mirrored.js
+++ mirrored.js
@@ -13,7 +13,8 @@ function draw() {
   y.push(mouseY); // equivalent to append(y, mouseY)
 
   for (var i = 0; i < 60; i = i + 5) {
-    ellipse(x[i], y[i], 1 + (x.length - 2*i));
+    ellipse(x[i], y[i], 1 + (x.length - 2*i))
+    ellipse(width-x[i],height-y[i],1+(x.length-2*i));
   }
 
   x = x.slice(-50); // keep the last 50 x values
